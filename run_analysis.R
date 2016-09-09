9b2f88b184b9db0d2ada225a2dcd5e0a62b0be06
Francisco Jaramillo Aguilar <francisco.jaramilloaguilar@gmail.com>
2016-09-09 17:34:44 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,27 +1,35 @@
+############################################
+# 1.
+
 library(data.table)
+library(dplyr)
+library(plyr)
+library(reshape2)
+library(xlsx)
+############################################
+# 2.   Set directory for train sets
 setwd("C:/Users/Francisco/Documents/proyectos/John Hopkins/final/UCI HAR Dataset/train")
 ############################################
-#       Read Train sets                   
+# 3.       Read Train sets                   
 X_train <- fread("X_train.txt")           
 Y_train <- fread("Y_train.txt")
 sub_train <- fread("subject_train.txt")
-#ba_x_t <- fread("body_acc_x_train.txt")
 ############################################
 
 ############################################
-#       Activity Names
+# 4.     Activity Names for train sets
 rnames <- factor(Y_train$V1, labels = c("WALKING", "WALKING_UPSTAIRS",
                  "WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING"))
 ############################################
 
 ############################################
-#       Bind Train sets
+# 5.     Bind Train sets
 Train_DF <- cbind(rnames, X_train)
 Train_DF <- cbind(sub_train, Train_DF)
 ############################################
 
 #############################################
-#     Names
+# 6.     Set Names for train data frame
 names_train <- fread("features.txt")
 names_train <- subset(names_train, select = V2)
 names <- as.character(names_train$V2)
@@ -29,11 +37,11 @@ names(Train_DF)[3:563] <- c(names)
 names(Train_DF)[1:2] <- c("Subject", "Activity")
 
 ##############################################
-
-library(data.table)
+###############################################
+# 7.   Set directory for test sets
 setwd("C:/Users/Francisco/Documents/proyectos/John Hopkins/final/UCI HAR Dataset/test")
 ############################################
-#       Read Train sets                   
+# 8.      Read test sets                   
 X_test <- fread("X_test.txt")           
 Y_test <- fread("Y_test.txt")
 sub_test <- fread("subject_test.txt")
@@ -41,19 +49,19 @@ sub_test <- fread("subject_test.txt")
 ############################################
 
 ############################################
-#       Activity Names
+#  9.     Activity Names for test sets
 rnames_t <- factor(Y_test$V1, labels = c("WALKING", "WALKING_UPSTAIRS",
               "WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING"))
 ############################################
 
 ############################################
-#       Bind Test sets
+# 10.     Bind Test sets
 Test_DF <- cbind(rnames_t, X_test)
 Test_DF <- cbind(sub_test, Test_DF)
 ############################################
 
 #############################################
-#     Names
+# 11.     Set Names for test data frame
 names_test <- fread("features.txt")
 names_test <- subset(names_test, select = V2)
 names_t <- as.character(names_test$V2)
@@ -61,19 +69,17 @@ names(Test_DF)[3:563] <- c(names_t)
 names(Test_DF)[1:2] <- c("Subject", "Activity")
 
 ##############################################
+# 12.   Set directory for tidy data set
 setwd("C:/Users/Francisco/Documents/proyectos/John Hopkins/final")
-
-library(data.table)
-library(dplyr)
-library(plyr)
-library(reshape2)
-library(xlsx)
-
+##############################################
+##############################################
+# 13. Bind Train and test sets
 prueba <- rbind(Train_DF, Test_DF)
 prueba <- prueba[order(Subject),]
 names(prueba)
-
-######################################
+###############################################
+###############################################
+# 14. Change variable names for tidy set
 feat <- fread("features.txt")
 nombres <- feat$V2
 m <- 9
@@ -86,27 +92,22 @@ for(i in 1:m){
 }
 names(prueba)[3:563] <- nombres
 head(nombres,-50)
-#######################################
+#################################################
+#################################################
+# 15.  Subset for mean and std variables
 
 mpatt <- c("mean", "std")
-
 nomsel_mean <- grep(pattern = mpatt[1], nombres, value=TRUE)
 nomsel_std <- grep(pattern = mpatt[2], nombres, value=TRUE)
-
 nomsel <-c("Subject","Activity",nomsel_mean, nomsel_std)
 nomsel_1 <- c(nomsel_mean, nomsel_std)
 ext <- prueba[, nomsel, with = FALSE]
-
 ext$Subject <- as.factor(ext$Subject)
-
-#ext_1 <- prueba[, nomsel_1, with = FALSE]
-########################################
-
+###################################################
+# 16.   Calculate mean
 setkey(prueba, Subject, Activity)
-
+###################################################
+# 17. Create Tidy data and write tidy.txt
 ext.melted <- melt(ext, id = c("Subject", "Activity"))
 tidy <-dcast(ext.melted, Subject + Activity ~ variable, mean)
-
-########################################
-
 write.xlsx(tidy, "C:/Users/Francisco/Documents/proyectos/John Hopkins/final/tidy.xlsx")
